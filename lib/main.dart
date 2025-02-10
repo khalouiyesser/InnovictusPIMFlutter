f4a03d703fc3313b205bd137c68eeb9308454de5
shaymaouerhani <115483648+shayma-ouerhani@users.noreply.github.com>
2025-02-10 12:33:33 +0100

    edit profile

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: NewPasswordPage(), // Affiche le Dashboard directement
+      home: BottomNavBarExample(), // Affiche le Dashboard directement
     );
   }
 }
