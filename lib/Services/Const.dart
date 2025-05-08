e8a079aac072b9c3ca8f03c7aea50d6e3dec5221
YassineAjb <yassine.ajbouni@esprit.tn>
2025-05-08 22:57:54 +0100

    sign up tekhdem

--- lib/Services/Const.dart
+++ lib/Services/Const.dart
@@ -1,12 +1,12 @@
 class Const {
   //final String url = "http://192.168.1.122:3000"; // kahwetkom 1
-  final String url = "http://192.168.1.164:3009"; //dar shayma
+  final String url = "http://172.16.10.33:3009"; //dar shayma
   //final String url = "http://192.168.93.55:3009"; // Dar yesser
   //final String url = "http://192.168.1.114:3009"; // dar hadhemi
 
   // final String url = "http://10.0.2.2:3009"; // Dar yesser
-  final String urlSocket = "http://192.168.1.164:3000";
-  final String urlBlockChain = "http://192.168.1.164:5000";
+  final String urlSocket = "http://172.16.10.33:3000";
+  final String urlBlockChain = "http://172.16.10.33:5000";
 }
 
 class SessionConstants {
