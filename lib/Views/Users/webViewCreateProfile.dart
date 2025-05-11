40fa969b752b193be34d4c9bf636f1d14b93451b
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2025-05-11 05:01:29 +0100

    create profile completed

--- lib/Views/Users/webViewCreateProfile.dart
+++ lib/Views/Users/webViewCreateProfile.dart
@@ -147,7 +147,6 @@ class _WebViewPageYesserState extends State<WebViewPageYesser> {
                         _isLoading = false;
                         _statusMessage = result['message'] ?? 'Profil créé avec succès';
                       });
-t
                       Navigator.of(context).pop();
                       Navigator.of(context).pop();
                       Navigator.of(context).pushReplacement(
